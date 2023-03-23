508fbff2351849159b9667e7de2df1d845086199
DedeSnrwn13 <sunarwandede13@gmail.com>
2023-03-23 15:47:47 +0700

    feat: helo world

---
+++ 1_aplikasi_hello_world/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Aplikasi Hello World"),
+        ),
+        body: Center(
+            child: Container(
+                color: Colors.lightBlue,
+                width: 150,
+                height: 50,
+                child: Text(
+                  "Saya sedang melatih kemamanpuan flutter saya.",
+                  textAlign: TextAlign.center,
+                ))),
+      ),
+    );
+  }
+}
